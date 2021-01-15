f10d6d939837d5b555dece293e8f8b8e2a9e7a87
alyssaurbanski23 <831554@mystma.org>
2021-01-15 16:27:07 +0000

    test unit 2 try 1

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-#WRITE YOUR CODE IN THIS FILE
+def waterState(x, y, z):
+    if x == 10:
+        return solid
+    elif y == 60:
+        return liquid
+    else:
+        return gas
+
+print(waterState( 10, 60, 419))
+
+
+def isDozen(x, y):
+    if x / y:
+        return True 
+    
+    else:
+        return False
+
+print(isDozen(9, 3))
+
+def toGerman(x):
+    if x == ja:
+        return True
+    else:
+        return False
+
+print(toGerman(ja))
+
+
+def stopLight(x, y, z):
+    if x == go:
+        return green
+    elif y == slowdown:
+        return yellow
+    else:
+        return red
+
+print(stopLights(go, slowdown, stop))
